06c138d2917bfc82919241d961d2319a95ee9f20
Aubrey Howell <simplyaubs@gmail.com>
2014-03-09 13:40:54 -0600

    I'm an idiot and forgot to commit often...the drunkard is now singing.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,31 +1,27 @@
-=begin
-Write a program that prints out verses of "99 bottles of beer on the wall", according to the following rules:
+puts "Which verse should I sing of 99 Bottles?"
+verse_number = gets.chomp
 
-* The user must type "sing 88", where 88 is any positive number
-* If the user runs the program without typing anything, print "I need to know how many bottles to sing!"
-* If the user runs the program and specifies a verse with "sing <number>", print all the lines from the song starting at that verse
-* If the user types anything but "sing <number>", show an error and exit
-=end
+if verse_number == ""
+  puts "I need to know how many bottles to sing!"
+else
+  verse_number = verse_number.to_i
 
+  while verse_number < 0
+    verse_number = verse_number.abs
+  end
 
-
-puts "How many rounds would you like to sing?"
-
-num_at_start = gets.chomp
-
-num_now = num_at_start.to_i
-
-while num_now > 2
-  puts "#{num_now} bottles of beer on the wall, #{num_now} bottles of beer."
-  puts "Take one down and pass it around,"
-
-  num_now -= 1
-
-  puts "#{num_now} bottles of beer on the wall."
-
+# Sing the song and subtract one bottle
+  while verse_number > 2
+    puts "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n"
+    verse_number -= 1
+    puts "Take one down and pass it around, #{verse_number} bottles of beer on the wall.\n"
+  end
+  if verse_number == 2
+    puts "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+    puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+  else
+    puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+    puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+  end
 end
-
-
-puts "2 bottles of beer on the wall, 2 bottles of beer!"
-puts "Take one down, pass it around, 1 bottle of beer on the wall!"
-puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
