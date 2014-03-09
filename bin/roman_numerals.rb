143d45516e71179b9892c42828042d2e2e87bb25
Aubrey Howell <simplyaubs@gmail.com>
2014-03-09 15:42:10 -0600

    User may enter Roman numeral, but not completely accurate yet.

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -1,11 +1,31 @@
 puts "Please enter a number."
 
 num = Integer(gets.chomp)
+output = ""
 
-if num < 4
-  puts "I" * num
+roman_num = {
+              1000 => "M",
+              900  => "CM",
+              500  => "D",
+              400  => "CD",
+              100  => "C",
+              90   => "XC",
+              50   => "L",
+              40   => "XL",
+              10   => "X",
+              9    => "IX",
+              5    => "V",
+              4    => "IV",
+              1    => "I"
+}
+
+while num > 0
+  roman_num.each do |key, value|
+    if num >= key
+      output = output + value
+      num -= key
+    end
+  end
 end
 
-if num == 5
-  puts "V"
-end
+puts output
